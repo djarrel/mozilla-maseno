99d045aaf1a4c89721bd731b97a2cf2c04b23410
ottagit <ottacc66@gmail.com>
2017-10-19 20:22:25 +0300

    preliminary site layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns a base title on a per-page basis
+  def full_title(page_title = '')
+    base_title = "Mozilla Maseno"
+    if page_title.empty?
+      base_title
+    else
+     "#{page_title} | #{base_title}"
+    end
+  end
 end
