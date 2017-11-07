192f14b06986ed6712a4ee83079ea4118ff34fae
ottagit <ottacc66@gmail.com>
2017-11-07 23:44:45 +0300

    index users, include test and layout links update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user_2 = users(:ben)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -38,4 +44,5 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  
 end
