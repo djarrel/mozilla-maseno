75b3b88c6343ae8570e7b723dfad03fd942c3937
ottagit <ottacc66@gmail.com>
2017-11-08 14:10:04 +0300

    test for all the layout links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:chris)
+  end
+
   test "layout_links" do
     get root_path
     assert_template "static_pages/home"
@@ -9,7 +13,26 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
     get contact_path
     assert_equal full_title("Contact"), "Contact | Mozilla Maseno"
+    get about_path
+    assert_equal full_title("About"), "About | Mozilla Maseno"
+    get signup_path
+    assert_equal full_title("Sign up"), "Sign up | Mozilla Maseno"
+    get login_path
+    assert_equal full_title("Log in"), "Log in | Mozilla Maseno"
+    # Behaviour for logged in user
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
