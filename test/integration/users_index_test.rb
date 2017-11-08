87f18179e52c2ef4f0f0458f6f9ab3c9aeac14e9
ottagit <ottacc66@gmail.com>
2017-11-09 02:33:36 +0300

    test for delete links and user a/c deletion

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,16 +3,30 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:chris)
+    @admin_user = users(:chris)
+    @non_admin = users(:ben)
   end
 
-  test "index should include pagination" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete delete links" do
+    log_in_as(@admin_user)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
+    page1_of_users = User.paginate(page: 1)
+    page1_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
+      unless user == @admin_user
+        assert_select "a[href=?]", user_path(user), text: 'Delete account'
+      end
     end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a[href=?]', user_path(@admin_user), text: 'delete', count: 0
   end
 end
