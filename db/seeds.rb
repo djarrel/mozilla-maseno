1fc839b5d133ba03368eda6e62431352bcda7a8e
ottagit <ottacc66@gmail.com>
2017-11-09 14:05:06 +0300

    update test data with a/c activation attrs

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,15 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 User.create!(name: "Ben Ten", email: "ben.ten@mozillamaseno.com", password: "passpass",
-             password_confirmation: "passpass", admin: true)
+             password_confirmation: "passpass", admin: true, activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "user-#{n+1}@mozillamaseno.com"
   password = "passpass"
   password_confirmation = password
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password, 
+              activated: true, activated_at: Time.zone.now)
 end
 
