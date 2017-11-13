0b2fde68bacc1f17e09e84bd8290c05a02b32bee
ottagit <ottacc66@gmail.com>
2017-11-14 01:04:28 +0300

    prevent non-activated users from logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,16 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
 
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        msg = "Account not activated."
+        msg += "Check your email for the activation link."
+        flash[:warning] = msg
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Incorrect email or password.'
       render 'new'
