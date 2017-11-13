37831a897fc1520b5bfb3f589e32bbc89652be8e
ottagit <ottacc66@gmail.com>
2017-11-14 01:37:14 +0300

    add a/c activation to users signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup info" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -13,15 +17,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup info" do
+  test "valid signup info with a/c activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Valid Name", email: "email@valid.com",
                                         password: "password", password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    # Log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path('Invalid token', email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong email')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
     follow_redirect!
-    #assert_template 'users/show'
+    assert_template 'users/show'
     assert_not flash.empty?
-    #assert is_logged_in?
+    assert is_logged_in?
   end
 end
