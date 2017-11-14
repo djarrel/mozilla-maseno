65f788ef978c9a7d5a4405529d24a0ff3db22807
ottagit <ottacc66@gmail.com>
2017-11-15 00:59:16 +0300

    show only activated users in index page

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,16 +5,18 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin_user = users(:chris)
     @non_admin = users(:ben)
+    @page1_of_users = User.paginate(page: 1)
   end
 
   test "index as admin including pagination and delete delete links" do
     log_in_as(@admin_user)
+    assert @admin_user.activated?
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    page1_of_users = User.paginate(page: 1)
-    page1_of_users.each do |user|
+    @page1_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
+      assert user.activated?
       unless user == @admin_user
         assert_select "a[href=?]", user_path(user), text: 'Delete account'
       end
@@ -26,7 +28,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   test "index as non-admin" do
     log_in_as(@non_admin)
+    assert @non_admin.activated?
     get users_path
-    assert_select 'a[href=?]', user_path(@admin_user), text: 'delete', count: 0
+    @page1_of_users.each do |user|
+      unless user == @non_admin
+        assert_select "a[href=?]", user_path(user), text: user.name
+        assert user.activated?
+        assert_select 'a[href=?]', user_path(user), text: 'delete', count: 0
+      end
+    end
   end
 end
