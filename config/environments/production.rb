e507710b11f9bb8aaef364329274e88f484dbd04
ottagit <ottacc66@gmail.com>
2017-11-15 17:02:56 +0300

    assign host using assignment operator

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method =:smtp
-  host: "mozillamaseno.herokuapp.com"
-  config.action_mailer.default_url_options { host: host }
+  config.action_mailer.delivery_method = :smtp
+  host = "mozillamaseno.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
