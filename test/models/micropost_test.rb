e8e8c44507c7d414cfc9e14f75ede0059915bc7d
ottagit <ottacc66@gmail.com>
2017-11-18 02:09:44 +0300

    test 4 micropost model validations

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:chris)
+    @micropost = @user.microposts.build(content: "Test Micropost")
+  end
+
+  test "micropost should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not  @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = ""
+    assert_not @micropost.valid?
+  end
+
+  test "content should not exceed 140 characters" do
+    @micropost.content = "c" * 141
+    assert_not @micropost.valid?
+  end
 end
