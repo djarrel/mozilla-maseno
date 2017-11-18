a9f9468cfe33836bc02134987814ab77160078f3
ottagit <ottacc66@gmail.com>
2017-11-18 03:12:48 +0300

    test for destruction of dependent microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,4 +76,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "should destroy associated microposts" do
+    @user.save
+    @user.microposts.create!(content: "About to be destroyed")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
