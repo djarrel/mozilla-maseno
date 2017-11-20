907d4662a6f96cdb88b922f76cf92a8e426920dd
ottagit <ottacc66@gmail.com>
2017-11-21 01:11:52 +0300

    handle failed micropost submissions
    Suppress the feed entirely by assigning it
    an empty array.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Post sent"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
