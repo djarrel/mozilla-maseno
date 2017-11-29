12bd43732467f57059200bd52bb09b966c143715
ottagit <ottacc66@gmail.com>
2017-11-29 03:33:55 +0300

    tweak micropost pic size

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,7 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/mes.jpg", storage: :s3
+  has_attached_file :picture, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/mes.jpg", storage: :s3
   validates_attachment_size :picture, less_than: 10.megabytes
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
