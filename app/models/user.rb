486745d467353bc1632b19505aaef9a7ae44a66b
ottagit <ottacc66@gmail.com>
2017-11-30 01:34:03 +0300

    add following utility methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,6 +73,20 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # Utility methods for following
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     # Returns email in lowercase 
