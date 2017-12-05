0fffca9468757c551b5af64f6dfaec335b03c5a5
ottagit <ottacc66@gmail.com>
2017-12-05 16:47:51 +0300

    add basic access ctrl tests for relationships

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+ test "should redirect create when not logged in" do
+    assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when no logged in" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_url
+  end
 end
